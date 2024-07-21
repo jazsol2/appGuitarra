270165c7704186d475a6686b991e791c055ae404
JazminS <jazminsolis2@gmail.com>
2024-07-20 20:48:30 -0500

    Se agrega nuevo archivo

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import Header from "./components/Header"
+import { db } from "./data/db"
+import Guitar from "./components/Guitar"
+
+function App() {
+  const [data, setData]= useState(db)
+  
+  return (
+    <>
+     <Header/>
+     <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className = "row mt-5 ">
+        {data.map((guitar)=>{
+          return(
+          <Guitar
+                guitar={guitar}
+              />
+            )}
+        )}  
+        </div>
+     </main>   
+
+     <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+     </footer>
+    </>
+  )
+  }
+
+export default App
