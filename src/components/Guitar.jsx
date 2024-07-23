853b2b2e183e6c6057cffa2c8f5d1d2d1c8263b4
JazminS <jazminsolis2@gmail.com>
2024-07-22 21:23:14 -0500

    primer commit

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -2,12 +2,12 @@ export default function Guitar ({guitar}){
     const {id, name, image, description, price} = guitar
     return(
         <div className="row mt-5">
-        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div className="col-md-6 col-lg-4 my-4 row align-items-center"> 
             <div className="col-4">
-                <img className="img-fluid" src={`/img/${image}`} alt="imagen guitarra" />
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt= {`imagen guitarra`} />
             </div>
             <div className="col-8">
-                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}Lukather</h3>
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name} </h3>
                 <p>{description} </p>
                 <p className="fw-black text-primary fs-3">${price} </p>
                 <button 
